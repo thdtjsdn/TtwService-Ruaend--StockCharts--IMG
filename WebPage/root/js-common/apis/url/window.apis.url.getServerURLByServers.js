5c0982a5a457832df745de17db01b97f06cb7803
thdtjsdn <thdtjsdn@gmail.com>
2020-02-03 20:58:01 +0900

    Update window.apis.url.getServerURLByServers.js

--- WebPage/root/js-common/apis/url/window.apis.url.getServerURLByServers.js
+++ WebPage/root/js-common/apis/url/window.apis.url.getServerURLByServers.js
@@ -153,6 +153,8 @@
 		, SYS0320_WebServer : [ "http://localhost:49781", "http://localhost:49781" ]
 		//, SYS0320_APIServer : [ "http://dnsproxy.shop:49780", "http://dnsproxy.shop:49780" ]
 		//, SYS0320_WebServer : [ "http://dnsproxy.shop:49781", "http://dnsproxy.shop:49781" ]
+		//, SYS0320_APIServer : [ "http://thdtjsdn.shop:49780", "http://thdtjsdn.shop:49780" ]
+		//, SYS0320_WebServer : [ "http://thdtjsdn.shop:49781", "http://thdtjsdn.shop:49781" ]
 		//, SYS0320_APIServer : [ "http://116.33.62.146:49780", "http://116.33.62.146:49780" ]
 		//, SYS0320_WebServer : [ "http://116.33.62.146:49781", "http://116.33.62.146:49781" ]
 		//--------------------------------------------------[ E ] - 비지니스 시스템 서버;

06ccf069fa3fdecfd4caf9909999c9e86e594241
thdtjsdn <thdtjsdn@gmail.com>
2020-03-11 22:29:12 +0900

    Update HTTPServer_WebPage.sh

--- HTTPServer_WebPage/HTTPServer_WebPage.sh
+++ HTTPServer_WebPage/HTTPServer_WebPage.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 kill -9 `lsof -i TCP:49781 | awk '/LISTEN/ {print $2}'`
 
 #..\..\TtwPlatform-Binary--Linux-Ubuntu/Binary/NodeJS/exes100 --expose-gc ./HTTPServer_WebPage.js 49781
-exes100 --expose-gc ./HTTPServer_WebPage.js 49781
+exes100 /data/data/com.termux/files/home/Application/TtwService-Ruaend--StockCharts--IMG/HTTPServer_WebPage/HTTPServer_WebPage.js 49781

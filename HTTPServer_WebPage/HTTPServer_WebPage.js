a971b97cf14a5f8d9bfc91f56a7cacebb44d0f80
thdtjsdn <thdtjsdn@gmail.com>
2020-05-27 16:05:53 +0900

    Update HTTPServer_WebPage.js

--- HTTPServer_WebPage/HTTPServer_WebPage.js
+++ HTTPServer_WebPage/HTTPServer_WebPage.js
@@ -277,7 +277,7 @@ global.apis.server_http.globalServer__setMode__Dev__FileResCommonPath();
 
 				try
 				{
-					var path = "../WebPage/root/json/stock/" + q.name + "/" + q.date + ".json" ;
+					var path = "../WebPage/root/json/stock/" + o[ q.name ] + "/" + q.date + ".json" ;
 					if( global.REQUIRES.fs.existsSync( path ) )
 						global.apis.response.send_200_String( req, res, SUtilFsReadStream.getFile( path ).toString() );
 				}

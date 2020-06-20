6c6ea016f1b5d9c1f55d225193238861ec6bd390
thdtjsdn <thdtjsdn@gmail.com>
2020-06-21 04:04:18 +0900

    Update HTTPServer_WebPage.js

--- HTTPServer_WebPage/HTTPServer_WebPage.js
+++ HTTPServer_WebPage/HTTPServer_WebPage.js
@@ -125,7 +125,7 @@ global.apis.server_http.globalServer__setMode__Dev__FileResCommonPath();
 		//IP 차단;
 		//*/
 		if( -1 != req.client.remoteAddress.indexOf( "1.246.223.32" )
-			|| -1 != req.client.remoteAddress.indexOf( "54.36.148.99" )//프랑스 - 스팸;
+			|| -1 != req.client.remoteAddress.indexOf( "54.36.148." )//프랑스 - AS16276 OVH SAS;
 			|| -1 != req.client.remoteAddress.indexOf( "62.210.172.8" )
 			|| -1 != req.client.remoteAddress.indexOf( "66.249.65." )//미국 - AS15169 Google LLC;
 			|| -1 != req.client.remoteAddress.indexOf( "195.54.160.135" )//러시아 - Arkada LLC;

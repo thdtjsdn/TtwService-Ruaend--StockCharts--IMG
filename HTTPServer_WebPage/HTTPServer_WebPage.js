f905d7475f8098152d98a82413edc3f6f83f1a7a
thdtjsdn <thdtjsdn@gmail.com>
2020-06-23 19:19:18 +0900

    Update HTTPServer_WebPage.js

--- HTTPServer_WebPage/HTTPServer_WebPage.js
+++ HTTPServer_WebPage/HTTPServer_WebPage.js
@@ -122,7 +122,9 @@ global.apis.server_http.globalServer__setMode__Dev__FileResCommonPath();
 			|| -1 != req.client.remoteAddress.indexOf( "54.36.148." )//프랑스 - AS16276 OVH SAS;
 			|| -1 != req.client.remoteAddress.indexOf( "62.210.172.8" )//네덜란드 - ;
 			|| -1 != req.client.remoteAddress.indexOf( "66.249.65." )//미국 - AS15169 Google LLC;
-			|| -1 != req.client.remoteAddress.indexOf( "195.54.160.135." )//러시아 - Arkada LLC;
+			|| -1 != req.client.remoteAddress.indexOf( "192.99.4." )//캐나다 - OVH Hosting Inc.;
+			//|| -1 != req.client.remoteAddress.indexOf( "192.99.4.163" )//캐나다 - OVH Hosting Inc.;
+			|| -1 != req.client.remoteAddress.indexOf( "195.54.160.135" )//러시아 - Arkada LLC;
 		)
 		{
 			//global.apis.response.send_404( req, res );

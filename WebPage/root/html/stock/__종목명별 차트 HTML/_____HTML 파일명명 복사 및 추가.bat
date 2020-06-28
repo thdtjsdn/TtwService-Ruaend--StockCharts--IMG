0831bd3b4d3218e1d83c1c5504c3c92123bdbb2b
thdtjsdn <thdtjsdn@gmail.com>
2020-06-28 13:23:13 +0900

    Create _____HTML 파일명명 복사 및 추가.bat

---
+++ WebPage/root/html/stock/__종목명별 차트 HTML/_____HTML 파일명명 복사 및 추가.bat
@@ -0,0 +1,61 @@
+chcp 65001
+copy /y "금호석유화학.html" "금호석유.html"
+copy /y "한국석유공업.html" "한국석유.html"
+copy /y "브릿지바이오테라퓨틱스.html" "브릿지바이오.html"
+copy /y "브릿지바이오.html" "브릿지바이오테라퓨틱스.html"
+copy /y "포스코.html" "POSCO.html"
+
+copy /y "현대상선.html" "HMM.html"
+copy /y "동부제철.html" "KG동부제철.html"
+
+copy /y "NAVER.html" "NHN.html"
+copy /y "NAVER.html" "네이버.html"
+
+copy /y "삼화콘덴서공업.html" "삼화콘덴서.html"
+copy /y "삼화페인트공업.html" "삼화페인트.html"
+copy /y "현대엘리베이터.html" "현대엘리베이.html"
+copy /y "현대자동차.html" "현대차.html"
+copy /y "기아자동차.html" "기아차.html"
+
+copy /y "한국전력공사.html" "한국전력.html"
+copy /y "쌍용양회공업.html" "쌍용양회.html"
+
+copy /y "유니맥스글로벌.html" "이디티.html"
+copy /y "제이스테판.html" "에이루트.html"
+copy /y "유나이티드.html" "유나이티드제약.html"
+copy /y "동양물산기업.html" "동양물산.html"
+
+copy /y "한국프랜지공업.html" "한국프랜지.html"
+
+copy /y "나노메딕스.html" "이엔플러스.html"
+copy /y "이엔플러스.html" "나노메딕스.html"
+
+copy /y "서울식품공업.html" "서울식품.html"
+
+copy /y "한국주철관공업.html" "한국주철관.html"
+
+copy /y "효성 ITX.html" "효성ITX.html"
+
+copy /y "티비에이치글로벌.html" "TBH글로벌.html"
+
+copy /y "진양폴리우레탄.html" "진양폴리.html"
+
+copy /y "유진증권.html" "유진투자증권.html"
+
+copy /y "엔피씨.html" "NPC.html"
+
+copy /y "엘브이엠씨.html" "엘브이엠씨홀딩스.html"
+
+copy /y "현대종합상사.html" "현대상사.html"
+
+copy /y "케이티앤지.html" "KT&G.html"
+
+copy /y "화승인더스트리.html" "화승인더.html"
+
+copy /y "신세계I&C.html" "신세계 I&C.html"
+
+copy /y "케이티.html" "KT.html"
+
+copy /y "삼성화재해상보험.html" "삼성화재.html"
+
+copy /y "케이씨씨.html" "KCC.html"

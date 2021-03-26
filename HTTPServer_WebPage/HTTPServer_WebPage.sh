ecc52eb8abd437cc26660d940726606cca50809e
thdtjsdn <thdtjsdn@gmail.com>
2021-03-26 23:55:40 +0900

    D, N

--- HTTPServer_WebPage/HTTPServer_WebPage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $(dirname $0)
-
-kill -9 `lsof -i TCP:49781 | awk '/LISTEN/ {print $2}'`
-
-#..\..\TtwPlatform-Binary--Linux-Ubuntu/Binary/NodeJS/exes100 --expose-gc ./HTTPServer_WebPage.js 49781
-ulimit -n 2048
-exes100 /data/data/com.termux/files/home/Application/TtwService-Ruaend--StockCharts--IMG/HTTPServer_WebPage/HTTPServer_WebPage.js 49781
